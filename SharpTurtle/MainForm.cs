653cb89c8711519163bf1c77026320fdb58d264f
jjackson37 <turbojake37@gmail.com>
2017-01-13 08:42:09 +0000

    Removed button system

--- SharpTurtle/MainForm.cs
+++ SharpTurtle/MainForm.cs
@@ -13,79 +13,10 @@ namespace SharpTurtle
             InitializeComponent();
         }
 
-        private void buttonSelectColor_Click(object sender, EventArgs e)
-        {
-            ColorDialog selectColorDialog = new ColorDialog();
-            selectColorDialog.AllowFullOpen = true;
-            selectColorDialog.ShowHelp = true;
-            selectColorDialog.Color = buttonSelectedColor.BackColor;
-            if (selectColorDialog.ShowDialog() == DialogResult.OK)
-                buttonSelectedColor.BackColor = selectColorDialog.Color;
-        }
-
         private void mainForm_Load(object sender, EventArgs e)
         {
-            getButton().FlatAppearance.BorderSize = 5;
+            
         }
 
-        private void moveButtonClicked(object sender, EventArgs e)
-        {
-            getButton().FlatAppearance.BorderSize = 1;
-            Button buttonClicked = (Button)sender;
-            switch (buttonClicked.Name)
-            {
-                case "buttonUp":
-                    if (coordY != MinY)
-                    {
-                        coordY--;
-                        paintButton(getButton());
-                    }
-                    break;
-                case "buttonDown":
-                    if (coordY != MaxY)
-                    {
-                        coordY++;
-                        paintButton(getButton());
-                    }
-                    break;
-                case "buttonLeft":
-                    if (coordX != MinX)
-                    {
-                        coordX--;
-                        paintButton(getButton());
-                    }
-                    break;
-                case "buttonRight":
-                    if (coordX != MaxX)
-                    {
-                        coordX++;
-                        paintButton(getButton());
-                    }
-                    break;
-            }
-            getButton().FlatAppearance.BorderSize = 5;
-        }
-
-        private void paintButton(Button selectedButton)
-        {
-            if(paintCheckBox.Checked)
-            {
-                selectedButton.BackColor = buttonSelectedColor.BackColor;
-            }
-        }
-
-        private Button getButton()
-        {
-            Button returnButton = null;
-            string tileName = "button" + coordX + coordY;
-            foreach (Button loopButton in drawingPanel.Controls)
-            {
-                if (loopButton.Name.Equals(tileName))
-                {
-                    returnButton = loopButton;
-                }
-            }
-            return returnButton;
-        }
     }
 }
